a7e340f76e57da773943f4b06d7aa0b449fabfe5
ElvisGiz <elvis.gizatullin@tages.ru>
2022-10-27 14:48:42 +0500

    dd

--- api_testing_examples/requests.test.js
+++ api_testing_examples/requests.test.js
@@ -5,54 +5,83 @@ test("Swagger", async () => {
     .get(url + "/api/")
     .expectStatus(200);
 });
+//CRUD of templates
 test("/pforms_createTemplate", async () => {
   await spec()
-    .post(url + "/pforms_createTemplate?template_key=AUTOTEST&full_name=UPD")
-    .withFile("html", "./html.html")
+    .post(url + "/pforms_createTemplate?template_key=AUTOTEST&full_name=Test")
+    .withFile("template_body", "./html.html")
     .expectStatus(200);
 });
-test("/pForms_makePDF", async () => {
-  let new_post = {
-    template_key: "AUTOTEST",
-    data: {
-      VIN: "XWEDC41EBK0005314",
-    },
-  };
-
-  await spec()
-    .post(url + "/pForms_makePDF")
-    .withJson(new_post)
-    .expectStatus(200)
-    .expectBodyContains("uuid");
-});
-test("/pforms_getPDF", async () => {
-  await spec()
-    .get(url + "/pforms_getPDF?uuid=f04ea452-5132-4a3d-ad42-b0500ade0966")
-    .expectStatus(200)
-    .expectBodyContains("link");
-});
-test("/pForms_updateFormStatus", async () => {
-  let new_post = {
-    uuid: "e1ff37bd-239d-4ca2-a160-71e5933ad57c",
-    status: 3,
-  };
-  await spec()
-    .post(url + "/pForms_updateFormStatus")
-    .withJson(new_post)
-    .expectStatus(200);
-});
-
 test("/pforms_getTemplate", async () => {
   await spec()
     .get(url + "/pforms_getTemplate?template_key=AUTOTEST")
-    .expectStatus(200)
-    .expectBodyContains("id");
+    .expectStatus(200);
 });
 test("/pforms_getListTemplate", async () => {
   await spec()
     .get(url + "/pforms_getListTemplate")
     .expectStatus(200);
 });
+test("/pforms_updateTemplate", async () => {
+  await spec()
+    .patch(
+      url + "/pforms_updateTemplate?template_key=AUTOTEST&full_name=Update"
+    )
+    .withFile("template_body", "./html.html")
+    .expectStatus(200);
+});
+
+test("/pforms_makePDF", async () => {
+  let new_post = {
+    data: {
+      single_word: "XWEDC41EBK0005314",
+    },
+  };
+  await spec()
+    .post(url + "/pforms_makePDF?template_key=AUTOTEST")
+    .withJson(new_post)
+    .expectStatus(200)
+    .expectBodyContains("uuid");
+});
+test("/pforms_getPDF", async () => {
+  await spec()
+    .get(url + "/pforms_getPDF?uuid=a430394d-42eb-4b90-bbca-343b369965d0")
+    .expectStatus(200)
+    .expectBodyContains("link");
+});
+test("/pforms_updateFormStatus", async () => {
+  let new_post = {
+    uuid: "a430394d-42eb-4b90-bbca-343b369965d0",
+    status: 3,
+  };
+  await spec()
+    .post(url + "/pforms_updateFormStatus")
+    .withJson(new_post)
+    .expectStatus(200);
+});
+
+test("/pforms_getListPDF", async () => {
+  let new_post = ["a430394d-42eb-4b90-bbca-343b369965d0"];
+  await spec()
+    .post(url + "/pforms_getListPDF?form_status=3")
+    .withJson(new_post)
+    .expectStatus(200);
+});
+test("/pforms_getPDF", async () => {
+  await spec()
+    .get(url + "/pforms_getPDF?uuid=a430394d-42eb-4b90-bbca-343b369965d0")
+    .expectStatus(200)
+    .expectBodyContains("link");
+});
+
+test("/pforms_updatePDF", async () => {
+  let new_post = {};
+  await spec()
+    .post(url + "/pforms_updatePDF?uuid=a430394d-42eb-4b90-bbca-343b369965d0")
+    .withJson(new_post)
+    .expectStatus(200);
+});
+
 test("/pforms_deleteTemplate", async () => {
   await spec()
     .delete(url + "/pforms_deleteTemplate?template_key=AUTOTEST")
